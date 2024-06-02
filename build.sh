7e3e596bdcba562edb31490d89e3becf53960392
Tonton <115675957+Tonklaistonton@users.noreply.github.com>
2024-06-02 11:32:56 +0700

    Update build.sh
    sing

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #envsetup
 source build/envsetup.sh
+# singasong
+curl https://raw.githubusercontent.com/sounddrill31/crave_aosp_builder/signing/scripts/signing-script.sh | bash
 # lunch command 
 lunch derp_oscar-user
 export TZ=Asia/Dhaka
