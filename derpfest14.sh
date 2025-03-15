b069c7da6ac229934f1b22b6d4273bd2853809cb
Tonton <phanuwatgamer158@gmail.com>
2025-03-15 11:00:47 +0700

    Update derpfest15.sh

--- derpfest14.sh
+++ derpfest14.sh
@@ -3,9 +3,11 @@ echo
 echo "--------------------------------------"
 echo "    derpfest14 14.0 Buildbot          "
 echo "                  by                  "
+echo "            Tonklaistonton            "
+echo "                  Thx                 " 
 echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
-repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 14
+repo init -u https://github.com/DerpFest-AOSP/manifest.git -b 15 --git-lfs
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 git clone https://github.com/phhgsi/scripts.git && chmod u+x scripts/sync.sh && ./scripts/sync.sh
