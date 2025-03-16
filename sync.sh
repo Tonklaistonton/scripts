a7bd9814586e1a17f090e2ab1c12688e43ff2c9d
Tonton <phanuwatgamer158@gmail.com>
2025-03-16 08:56:11 +0700

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -10,23 +10,23 @@ echo "            ajaysinghsati             "
 echo "--------------------------------------"
 echo
 rm -rf device/realme/oscar
-git clone https://github.com/Tonklaistonton/android_device_realme_oscarlineage.git -b derpcam device/realme/oscar
+git clone https://github.com/Tonklaistonton/android_device_realme_oscarlineage.git --depth=1 -b derpcam device/realme/oscar
 rm -rf device/realme/sm6375-common
-git clone https://github.com/Tonklaistonton/android_device_realme_sm6375-common.git -b derp device/realme/sm6375-common
+git clone https://github.com/Tonklaistonton/android_device_realme_sm6375-common.git --depth=1 -b derp device/realme/sm6375-common
 rm -rf kernel/realme/sm6375
-git clone https://github.com/Tonklaistonton/android_kernel_realme_sm6375.git -b lineage-22.1  kernel/realme/sm6375
+git clone https://github.com/Tonklaistonton/android_kernel_realme_sm6375.git --depth=1 -b lineage-22.1  kernel/realme/sm6375
 rm -rf vendor/realme/oscar
-git clone https://github.com/TheMuppets/proprietary_vendor_realme_oscar.git -b lineage-22.1 vendor/realme/oscar
+git clone https://github.com/TheMuppets/proprietary_vendor_realme_oscar.git --depth=1 -b lineage-22.1 vendor/realme/oscar
 rm -rf vendor/realme/sm6375-common
-git clone https://github.com/TheMuppets/proprietary_vendor_realme_sm6375-common.git -b lineage-22.1 vendor/realme/sm6375-common
+git clone https://github.com/TheMuppets/proprietary_vendor_realme_sm6375-common.git --depth=1 -b lineage-22.1 vendor/realme/sm6375-common
 rm -rf hardware/oplus
-git clone https://github.com/Tonklaistonton/android_hardware_oplus.git -b derp hardware/oplus
+git clone https://github.com/Tonklaistonton/android_hardware_oplus.git --depth=1 -b derp hardware/oplus
 rm -rf vendor/oplus/lib
 git clone https://github.com/phhgsi/oplus_lib.git vendor/oplus/lib
 rm -rf vendor/oplus/camera
-git clone https://gitlab.com/pjgowtham/proprietary_vendor_oplus_camera.git -b lineage-22.0 vendor/oplus/camera
+git clone https://gitlab.com/pjgowtham/proprietary_vendor_oplus_camera.git --depth=1 -b lineage-22.0 vendor/oplus/camera
 rm -rf packages/apps/RealmeParts
-git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-21 packages/apps/RealmeParts
+git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git --depth=1 -b lineage-21 packages/apps/RealmeParts
 # Run build script 
 chmod u+x scripts/build.sh
 ./scripts/build.sh
