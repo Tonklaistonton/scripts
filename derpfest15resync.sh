7f7fd41d389ba260e5c6f10bb373fe728ebefc4d
Tonton <phanuwatgamer158@gmail.com>
2025-03-16 07:05:04 +0700

    Create derpfest15resync.sh

---
+++ derpfest15resync.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo
+echo "--------------------------------------"
+echo "    derpfest14 14.0 Buildbot          "
+echo "                  by                  "
+echo "            Tonklaistonton            "
+echo "                  Thx                 " 
+echo "            ajaysinghsati             "
+echo "--------------------------------------"
+echo
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+chmod u+x scripts/sync.sh && ./scripts/sync.sh
